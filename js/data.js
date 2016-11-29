a8b7208db89b6d837de6b4f608bcea06c4349974
M. Fay Wu <mfaywu@ucla.edu>
2016-11-29 11:42:10 -0800

    Add knockout

---
+++ js/data.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function () {
+    /*var json = [{ "User_Name": "John Doe", "score": "10", "team": "1" }, { "User_Name": "Jane Smith", "score": "15", "team": "2" }, { "User_Name": "Chuck Berry", "score": "12", "team": "2" }];
+    console.log(json);
+    ko.applyBindings({
+        teams: json
+    });*/
+
+    var url = "https://spreadsheets.google.com/feeds/list/13Hpv0NLNMfiqWfA1kXzBjioYD5oVXSN5frzBAwnsuBw/od6/public/values?alt=json";
+    xmlhttp = new XMLHttpRequest();
+    var data;
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4) {
+            data = JSON.parse(xmlhttp.responseText).feed.entry;
+            console.log(data);
+            console.log(data[0].id.$t);
+            ko.applyBindings({
+                events: ko.observableArray(data)
+            });
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send(null);
+});
